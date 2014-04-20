c44431732fc5c62ba33fe5e861a53e5e8ec827ca
Eduardo Florencio <edudesvars@gmail.com>
2014-04-20 11:12:53 -0400

    Added qunit and a js that makes the error only show when the user is not writing something in the input

---
+++ lists/static/list.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function ($) {
+    $("#id_text").on('keypress', function () {
+        $('.has-error').hide();
+    });
+    
+    $("#id_text").on('click', function() {
+        $('.has-error').hide();
+    });
+});
+
